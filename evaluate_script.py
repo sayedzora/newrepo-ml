4a8902fa691476241efa442e6a3546491669f181
sawradip <sawradip0@gmail.com>
2022-04-24 01:29:14 +0600

    cleaned evaluation script

--- evaluate_script.py
+++ evaluate_script.py
@@ -38,43 +38,20 @@ def main(load = None):
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
     num_classes = 100
-    # train_dataset = LabeledDataset(root='/labeled', split="training", transforms=get_transform(train=True))
-    # train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=1, shuffle=True, num_workers=2, collate_fn=utils.collate_fn)
 
     valid_dataset = LabeledDataset(root='/labeled', split="validation", transforms=get_transform(train=False))
     valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=1, shuffle=False, num_workers=2, collate_fn=utils.collate_fn)
 
     model = get_model(num_classes)
-    model.to(device)
-
-    # params = [p for p in model.parameters() if p.requires_grad]
-    # optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
-    # lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
-    # prev_epoch = 0
     if load:
         checkpoint = torch.load(load)
         model.load_state_dict(checkpoint['model_state_dict'])
-        # optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-        # prev_epoch = checkpoint['epoch']
-        print("Model Loaded")
+    model.to(device)
+    print("Model Loaded")
 
-    # num_epochs = 10
 
-    # for epoch in range(prev_epoch, prev_epoch + num_epochs):
-    #     # train for one epoch, printing every 10 iterations
-    #     train_one_epoch(model, optimizer, train_loader, device, epoch, print_freq=10)
-    #     # update the learning rate
-    #     lr_scheduler.step()
-    #     # evaluate on the test dataset
     evaluate(model, valid_loader, device=device)
-
-    # torch.save({
-    #         'epoch': epoch,
-    #         'model_state_dict': model.state_dict(),
-    #         'optimizer_state_dict': optimizer.state_dict(),
-    #         }, f"epoch_{epoch}_weight.pth")
-
     print("Evaluation Completed")
 
 if __name__ == "__main__":
-    main("C:\practice_python\prev_epoch_0_weight.pth")
+    main(load = "C:\practice_python\prev_epoch_0_weight.pth")
